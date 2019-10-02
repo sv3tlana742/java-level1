aeeb85ee19ec9cc94241f6e2f4a79978fe0c6c1f
sv3tlana742 <lightrays.me@gmail.com>
2019-10-02 21:45:18 +0700

    Lesson 7 homework

--- src/Lesson7/Cat.java
+++ src/Lesson7/Cat.java
@@ -3,7 +3,7 @@ package Lesson7;
 public class Cat {
     private String name;
     private int appetite;
-    private boolean satiety;
+    private boolean satiety = false;
 
     public Cat(String name, int appetite) {
         this.name = name;
@@ -13,15 +13,15 @@ public class Cat {
     public void eat(Plate p) {
         if (p.decreaseFood(appetite)) {
             satiety = true;
-            System.out.println("Кот " + name + " съел " + appetite + " еды" + " => " + satiety);
+            System.out.println("Кот " + name + " съел " + appetite + " еды");
         } else {
             satiety = false;
-            System.out.println("Коту " + name + " не хватило еды" + " => " + satiety);
+            System.out.println("Коту " + name + " не хватило еды");
         }
     }
 
     public void info() {
-        if (satiety == true) {
+        if (satiety) {
             System.out.println("Кот " + name + " сытый");
         } else {
             System.out.println("Кот " + name + " голоден");
